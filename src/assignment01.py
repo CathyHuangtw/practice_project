2f5ff2746c04aecf3f15b7f2ef0a077b7789597f
Cathy Huang <cathyhaung.tw@gmail.com>
2017-01-14 16:18:58 -0800

    Add source code

---
+++ src/assignment01.py
@@ -0,0 +1,17 @@
+##=============assignment01_cathy_huang.py=======================
+## Author: Cathy Huang
+## Student ID: 91849
+## Date: 9/21/2016
+## Assignment: Python Program 1
+## ==============================================================
+
+ >>> def addItemToList (thelist):
+	thelist.append(input())
+	return thelist
+
+>>> friendList = ['cathy', 'jubbie', 'freyia']
+>>> s = addItemToList(friendList)
+helen
+>>> s
+['cathy', 'jubbie', 'freyia', 'helen']
+
