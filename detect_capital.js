cae45930cf3bcc424579b3c35f606c6a78220324
Cathy Huang <cathyhaung.tw@gmail.com>
2017-02-18 23:41:07 -0800

    start working on Question D

---
+++ detect_capital.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+
+let str = "FALLLDEW";
+
+var detectCapitalUse = (word) => {
+    let character;
+    let length = word.length;
+    let res = word.slice(1, length);
+    let ans;
+
+    character = word.charAt(0);
+    if (character == character.toUpperCase()) {
+        if (res == res.toUpperCase() || res == res.toLowerCase()){
+            ans = true;
+        }else ans = false;
+    }
+    if (character == character.toLowerCase()){
+        if (res == res.toLowerCase()){
+            ans = true;
+        }else ans = false;
+    }
+
+    return ans;
+
+};
+
+console.log(detectCapitalUse(str));
